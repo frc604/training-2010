2362ad3b28ae398400ad1824a925d0ebe74b8d8f
Michael Smith <mdsmtp@gmail.com>
2013-10-23 20:47:35 -0700

    Switch to joystick controllers

--- src/com/_604robotics/robot2010/modes/TeleopMode.java
+++ src/com/_604robotics/robot2010/modes/TeleopMode.java
@@ -4,14 +4,17 @@ import com._604robotics.robotnik.coordinator.Coordinator;
 import com._604robotics.robotnik.coordinator.connectors.Binding;
 import com._604robotics.robotnik.coordinator.connectors.DataWire;
 import com._604robotics.robotnik.module.ModuleManager;
-import com._604robotics.robotnik.prefabs.controller.xbox.XboxController;
+import com._604robotics.robotnik.prefabs.controller.joystick.JoystickController;
+import com._604robotics.robotnik.prefabs.trigger.TriggerOr;
+import com._604robotics.robotnik.trigger.TriggerAccess;
 
 public class TeleopMode extends Coordinator {
-    private final XboxController driveController = new XboxController(1);
+    private final JoystickController leftDriveStick = new JoystickController(1);
+    private final JoystickController rightDriveStick = new JoystickController(1);
     
     public TeleopMode () {
-        this.driveController.leftStick.Y.setDeadband(0.2);
-        this.driveController.rightStick.Y.setDeadband(0.2);
+        this.leftDriveStick.axisY.setDeadband(0.2);
+        this.rightDriveStick.axisY.setDeadband(0.2);
     }
     
     public void apply (ModuleManager modules) {
@@ -19,13 +22,15 @@ public class TeleopMode extends Coordinator {
         {
             /* Drive */
             {
-                this.fill(new DataWire(modules.getModule("Drive").getAction("Drive"), "left", driveController.leftStick.Y));
-                this.fill(new DataWire(modules.getModule("Drive").getAction("Drive"), "right", driveController.rightStick.Y));
+                this.fill(new DataWire(modules.getModule("Drive").getAction("Drive"), "left", leftDriveStick.axisY));
+                this.fill(new DataWire(modules.getModule("Drive").getAction("Drive"), "right", rightDriveStick.axisY));
             }
             
             /* Shifting */
             {
-                this.bind(new Binding(modules.getModule("Shifter").getAction("High Gear"), driveController.buttons.RT));
+                this.bind(new Binding(modules.getModule("Shifter").getAction("High Gear"), new TriggerOr(new TriggerAccess[] {
+                    leftDriveStick.buttons.Button1, rightDriveStick.buttons.Button1
+                })));
             }
         }
     }
