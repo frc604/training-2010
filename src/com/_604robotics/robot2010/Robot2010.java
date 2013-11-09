0e9b4d8ee0073873a2fed85eb4a29db174214560
Michael Smith <mdsmtp@gmail.com>
2013-11-08 17:21:49 -0800

    Register modules

--- src/com/_604robotics/robot2010/Robot2010.java
+++ src/com/_604robotics/robot2010/Robot2010.java
@@ -2,6 +2,8 @@ package com._604robotics.robot2010;
 
 import com._604robotics.robot2010.modes.TeleopMode;
 import com._604robotics.robot2010.modules.Drive;
+import com._604robotics.robot2010.modules.Kicker;
+import com._604robotics.robot2010.modules.Regulator;
 import com._604robotics.robot2010.modules.Shifter;
 import com._604robotics.robotnik.Robot;
 import com._604robotics.robotnik.coordinator.ModeMap;
@@ -12,6 +14,9 @@ public class Robot2010 extends Robot {
         this.set(new ModuleMap() {{
             add("Drive", new Drive());
             add("Shifter", new Shifter());
+            add("Regulator", new Regulator());
+            add("Kicker", new Kicker());
+            add("Piston", new Piston());
         }});
         
         this.set(new ModeMap() {{
